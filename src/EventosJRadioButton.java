78ac209b491560c7380a40d43066b61e0c77cdbd
Local Host Tech <contacto@localhost.com.mx>
2015-09-14 17:44:36 -0500

    EventosJRadioBox.java cambiado por EventosJRadioButton.java

--- src/EventosJRadioButton.java
+++ src/EventosJRadioButton.java
@@ -11,12 +11,12 @@ import javax.swing.JRadioButton;
  *
  * @author Javier
  */
-public class EventosJRadioBox extends javax.swing.JDialog {
+public class EventosJRadioButton extends javax.swing.JDialog {
 
     /**
      * Creates new form EventosJRadioBox
      */
-    public EventosJRadioBox(java.awt.Frame parent, boolean modal) {
+    public EventosJRadioButton(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -136,20 +136,21 @@ public class EventosJRadioBox extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(EventosJRadioBox.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EventosJRadioButton.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(EventosJRadioBox.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EventosJRadioButton.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(EventosJRadioBox.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EventosJRadioButton.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(EventosJRadioBox.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EventosJRadioButton.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                EventosJRadioBox dialog = new EventosJRadioBox(new javax.swing.JFrame(), true);
+                EventosJRadioButton dialog = new EventosJRadioButton(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
--- src/Principal.java
+++ src/Principal.java
@@ -102,7 +102,7 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        new EventosJRadioBox(this, true).setVisible(true);
+        new EventosJRadioButton(this, true).setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
